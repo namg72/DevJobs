9f7083f2a8bb0765b32c5f510b82004747c23e7e
namg72 <namg72@gmail.com>
2024-07-26 15:03:14 +0200

    Creados mensajes de error

--- app/Livewire/CrearVacante.php
+++ app/Livewire/CrearVacante.php
@@ -8,6 +8,32 @@ use Livewire\Component;
 
 class CrearVacante extends Component
 {
+
+    public $titulo;
+    public $salario;
+    public $categoria;
+    public $empresa;
+    public $ultimo_dia;
+    public $descripcion;
+    public $imagen;
+
+
+    protected $rules = [
+        'titulo' => 'required|string',
+        'salario' => 'required',
+        'categoria' => 'required',
+        'empresa' => 'required|string',
+        'ultimo_dia' => 'required',
+        'descripcion' => 'required',
+        'imagen' => 'required',
+    ];
+
+    public function crearVacante()
+    {
+
+        $datos =  $this->validate();
+    }
+
     public function render()
     {
         //consulta BD para pasarla a la vistga
--- resources/views/livewire/crear-vacante.blade.php
+++ resources/views/livewire/crear-vacante.blade.php
@@ -1,14 +1,18 @@
-<form class="mdw-1/2 space-y-5">
+<form class="mdw-1/2 space-y-5" wire:submit.prevent='crearVacante'>
     <div>
         <x-input-label for="titulo" :value="__('Título Vacante')" class="uppercase" />
-        <x-text-input id="email" class="block mt-1 w-full" type="text" name="titulo" :value="old('titulo')"
+        <x-text-input id="titulo" class="block mt-1 w-full" type="text" wire:model="titulo" :value="old('titulo')"
             placeholder="Título vacante" />
+        @error('titulo')
+
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
         <x-input-label for="salario" :value="__('Salario Mensual')" class="uppercase mb-1" />
         <select class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
-            name="salario" id="salario">
+            wire:model="salario" id="salario">
 
             <option value="">-- Selecciona un salario --</option>
             @foreach ($salarios as $salario )
@@ -17,12 +21,16 @@
 
 
         </select>
+        @error('salario')
+
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
         <x-input-label for="categoria" :value="__('Categoría')" class="uppercase mb-1" />
         <select class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full"
-            name="categoria" id="categoria">
+            wire:model="categoria" id="categoria">
 
             <option value="">-- Selecciona una categoría --</option>
             @foreach ($categorias as $categoria)
@@ -31,31 +39,49 @@
 
 
         </select>
+        @error('categoria')
+
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
         <x-input-label for="empresa" :value="__('Empresa')" class="uppercase" />
-        <x-text-input id="text" class="block mt-1 w-full" type="text" name="empresa" :value="old('empresa')"
+        <x-text-input id="text" class="block mt-1 w-full" type="text" wire:model="empresa" :value="old('empresa')"
             placeholder="Empresa" />
+        @error('empresa')
+
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
 
     <div>
         <x-input-label for="ultimo_dia" :value="__('último dia para inscribirse')" class="uppercase" />
-        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" name="ultimo_dia"
+        <x-text-input id="ultimo_dia" class="block mt-1 w-full" type="date" wire:model="ultimo_dia"
             :value="old('ultimo_dia')" />
+        @error('ultimo_dia')
+
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
     </div>
     <div>
         <x-input-label for="descripción" :value="__('Descripción Puesto')" class="uppercase mb-1" />
         <textarea class="border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm w-full h-72"
-            name="descripcion" id="descripcion">
+            wire:model="descripcion" id="descripcion">
 
         </textarea>
+        @error('descripcion')
 
+        <livewire:mostrar-alerta :message="$message" />
+        @enderror
         </select>
     </div>
     <div>
         <x-input-label for="imagen" :value="__('Imagen')" class="uppercase" />
-        <x-text-input id="imagen" class="block mt-1 w-full" type="file" name="imagen" placeholder="Imagen" />
+        <x-text-input id="imagen" class="block mt-1 w-full" type="file" wire:model="descripcion" placeholder="Imagen" />
     </div>
 
+    <div>
+        <x-primary-button>Crear Vacante</x-primary-button>
+    </div>
 </form>
